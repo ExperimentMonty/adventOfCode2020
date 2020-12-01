9318ffd75290c8f1a1ee661cfc6348f1a21c6ed3
John Montgomery <johnbmontgomery@gmail.com>
2020-12-01 14:33:16 -0500

    Solve second part of day 1

--- Day1.go
+++ Day1.go
@@ -29,11 +29,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Solves first part
 	for i := 0; i < len(values); i++ {
 		for j := i+1; j < len(values); j++ {
 			//fmt.Println(values[i]+values[j])
 			if v:= values[i]+values[j]; v == 2020 {
 				fmt.Println(values[i]*values[j])
+			}
+		}
+	}
+
+	// Solves second part
+	for i, val1 := range values {
+		for j, val2 := range values[i+1:] {
+			for _, val3 := range values[j+1:] {
+				if val1 + val2 + val3 == 2020 {
+					fmt.Println(val1*val2*val3)
+				}
 			}
 		}
 	}
