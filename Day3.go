e7adf3404c6badb38d5919c95d16b4f7ba41fe03
John Montgomery <johnbmontgomery@gmail.com>
2020-12-03 14:25:51 -0500

    Solution to Day3-2

--- Day3.go
+++ Day3.go
@@ -24,7 +24,7 @@ func main() {
 
 	// Solution to part 1, right 3, down 1
 	// index is x, y, second step will be x+3, y+1, etc.
-	index := [2]int{0,0}
+	index := [2]int{0, 0}
 	treeCount := 0
 
 	for index[1] < len(treeMap) {
@@ -36,6 +36,34 @@ func main() {
 	}
 
 	println(treeCount)
+
+	// Solution to part 2, multiple slopes multiplied together
+	// Basically same as above, but we'll loop through a slice of slopes
+	slopes := [][]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	treeMultiple := 1
+	for _, slope := range slopes {
+		index = [2]int{0, 0}
+		treeCount = 0
+		for index[1] < len(treeMap) {
+			if treeMap[index[1]][index[0]] == '#' {
+				treeCount++
+			}
+			index[0] = (index[0] + slope[0]) % width
+			index[1] += slope[1]
+		}
+
+		treeMultiple *= treeCount
+
+	}
+
+	println(treeMultiple)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
