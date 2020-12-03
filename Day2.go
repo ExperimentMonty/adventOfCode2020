fa4133a388f79283c817d19e0aef37fdad03e5cc
John Montgomery <johnbmontgomery@gmail.com>
2020-12-02 20:51:18 -0500

    Solve Day2-2 and auto-format file

--- Day2.go
+++ Day2.go
@@ -16,6 +16,7 @@ func main() {
 	defer file.Close()
 
 	validCount := 0
+	validCount2 := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -27,15 +28,20 @@ func main() {
 		if validPassword(policy, password) {
 			validCount++
 		}
+
+		if validPassword2(policy, password) {
+			validCount2++
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 	println(validCount)
+	println(validCount2)
 }
 
-func validPassword (policy string, password string) bool {
+func validPassword(policy string, password string) bool {
 	components := strings.Split(policy, " ")
 	numberRange := strings.Split(components[0], "-")
 	minimum, _ := strconv.Atoi(numberRange[0])
@@ -47,6 +53,22 @@ func validPassword (policy string, password string) bool {
 
 	return count >= minimum && count <= maximum
 
+}
 
+func validPassword2(policy string, password string) bool {
+	components := strings.Split(policy, " ")
+	slots := strings.Split(components[0], "-")
+	slot1, _ := strconv.Atoi(slots[0])
+	slot2, _ := strconv.Atoi(slots[1])
 
+	// Ordinarily, we'd need to do this zero-index nonsense, but our separation left a preceding space
+	// No zero index
+	//slot1--
+	//slot2--
+
+	// Get this as a character instead of a string
+	character := components[1][0]
+
+	return (password[slot1] == character && password[slot2] != character) ||
+		(password[slot1] != character && password[slot2] == character)
 }
