8a619f397556dd2c8f4074e6e9525bd317516971
John Montgomery <johnbmontgomery@gmail.com>
2020-12-04 15:33:50 -0500

    Solve Day4-2. Really not proud of this one, but I am failing forward.

--- Day4.go
+++ Day4.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -15,8 +17,10 @@ func main() {
 	defer file.Close()
 
 	validCount := 0
+	validatedCount := 0
 
 	validFields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	validEyeColors := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
 	var passport = make(map[string]string)
 
 	scanner := bufio.NewScanner(file)
@@ -32,6 +36,7 @@ func main() {
 			}
 		} else {
 
+			// Solve for part 1
 			allFieldsPresent := true
 			for _, validField := range validFields {
 				if _, ok := passport[validField]; !ok {
@@ -41,6 +46,70 @@ func main() {
 
 			if allFieldsPresent {
 				validCount++
+
+				// Solve for part 2 (only relevant if part 1 is true)
+				allFieldsValid := true
+				for key, value := range passport {
+					if !allFieldsValid {
+						break
+					}
+
+					switch key {
+					case "byr":
+						year, _ := strconv.Atoi(value)
+						allFieldsValid = allFieldsValid && year >= 1920 && year <= 2002
+					case "iyr":
+						year, _ := strconv.Atoi(value)
+						allFieldsValid = allFieldsValid && year >= 2010 && year <= 2020
+					case "eyr":
+						year, _ := strconv.Atoi(value)
+						allFieldsValid = allFieldsValid && year >= 2020 && year <= 2030
+					case "hgt":
+						// Check suffix
+						if strings.HasSuffix(value, "in") {
+							measurement, err := strconv.Atoi(value[:len(value)-2])
+							if err != nil {
+								allFieldsValid = false
+							} else {
+								allFieldsValid = allFieldsValid && measurement >= 59 && measurement <= 76
+							}
+						} else if strings.HasSuffix(value, "cm") {
+							measurement, err := strconv.Atoi(value[:len(value)-2])
+							if err != nil {
+								allFieldsValid = false
+							} else {
+								allFieldsValid = allFieldsValid && measurement >= 150 && measurement <= 193
+							}
+						} else {
+							allFieldsValid = false
+						}
+					case "hcl":
+						found, err := regexp.MatchString("^#[0-9a-f]{6}$", value)
+						if err != nil {
+							allFieldsValid = false
+						}
+						allFieldsValid = allFieldsValid && found
+					case "ecl":
+						foundEyeColor := false
+						for _, eyeColor := range validEyeColors {
+							if value == eyeColor {
+								foundEyeColor = true
+							}
+						}
+						allFieldsValid = allFieldsValid && foundEyeColor
+					case "pid":
+						found, err := regexp.MatchString("^[0-9]{9}$", value)
+						if err != nil {
+							allFieldsValid = false
+						}
+						allFieldsValid = allFieldsValid && found
+
+					}
+
+				}
+				if allFieldsValid {
+					validatedCount++
+				}
 			}
 
 			passport = make(map[string]string)
@@ -49,4 +118,5 @@ func main() {
 	}
 
 	println(validCount)
+	println(validatedCount)
 }
