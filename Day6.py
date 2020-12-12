58d2b93454fd52b4cc87ab72c0379789e7d93d4a
John Montgomery <johnbmontgomery@gmail.com>
2020-12-12 12:39:30 -0500

    Solve Day6-2

--- Day6.py
+++ Day6.py
@@ -1,9 +1,13 @@
-
+from collections import defaultdict
 
 file_input = open("inputs/6.txt")
 
 group = set()
 count_sum = 0
+
+group_all = defaultdict(int)
+group_member_count = 0
+group_all_count = 0
 
 for line in file_input:
     line = line.strip()
@@ -11,12 +15,23 @@ for line in file_input:
         # Process group
         count_sum += len(group)
         group.clear()
+
+        # For part 2
+        group_all_count += len([k for k in group_all if group_all[k] == group_member_count])
+        group_member_count = 0
+        group_all.clear()
     else:
+        # Count group member
+        group_member_count += 1
         # Add questions to the group set
         for question in line:
             group.add(question)
+            group_all[question] += 1
+
 
 # Do final group
 count_sum += len(group)
+group_all_count += len([k for k in group_all if group_all[k] == group_member_count])
 
 print(count_sum)
+print(group_all_count)
